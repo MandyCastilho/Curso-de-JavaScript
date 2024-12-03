1772ccba96f5b0f4683cadbe306926be0cc0f573
MandyCastilho <165108783+MandyCastilho@users.noreply.github.com>
2024-12-03 15:16:18 -0300

    Aula 5 de JavaScript

---
+++ Exercicios/aula05/modelo01/script.js
@@ -0,0 +1,23 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = 14 //data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'manha.png'
+        document.body.style.background = '#fcd270'
+    }
+    else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE!
+        img.src = 'tarde.png'
+         document.body.style.background = '#a36a70'
+    }
+    else {
+        //BOA NOITE!
+        img.src = 'noite.png'
+        document.body.style.background = '#18384f'
+    }
+}
