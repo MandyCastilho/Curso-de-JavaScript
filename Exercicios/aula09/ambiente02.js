7d8ca28251c0733a9a618b25d82dcb40c24f2440
MandyCastilho <165108783+MandyCastilho@users.noreply.github.com>
2024-12-04 15:36:13 -0300

    Aula 9 de JavaScript
    Aprendendo variáveis compostas.

---
+++ Exercicios/aula09/ambiente02.js
@@ -0,0 +1,8 @@
+let num = [5, 8, 2, 9, 3]
+
+num.push(1)
+num.sort() // MOSTRA EM ORDEM CRESCENTE
+
+console.log(num)
+console.log(`O valor tem ${num.length} posições `)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
